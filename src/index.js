fb01915a8837ec95e4bf6a262a53ef5bb417388d
AntonAndrievkii <48820730+AntonAndrievkii@users.noreply.github.com>
2019-10-10 23:04:38 +0300

    Update index.js
    new

--- src/index.js
+++ src/index.js
@@ -38,7 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   // Init objEncode where will be stored encoded Morse : letter with space value
+    // Init array where will be stored 10-elem encoded morse which === 1 letter or space
+
+    let arrayWithCodeLetters = [];
+    let objEncode = {
+        '**********': ' '
+    }
+
+    // Create objEncode where are stored encoded Morse elements with letter value or space
+
+    for (let prop in MORSE_TABLE) {
+        let codedLetter = '';
+
+        // encode morse data with . = 10 and - = 11
+
+        for (let i = 0; i < prop.length; i++) {
+            if (prop[i] === '.') {
+                codedLetter += '10';
+            } else {
+                codedLetter += '11';
+            }
+        }
+
+        // add zeros to start string and pull this encoded morse to objEncode
+
+        if (codedLetter.length < 10) {
+            objEncode['0'.repeat(10 - codedLetter.length) + codedLetter] = MORSE_TABLE[prop];
+        } else {
+            objEncode[codedLetter] = MORSE_TABLE[prop];
+        }
+    }
+
+    // split letters into 10 elements to arrayWithCodeLetters
+
+    for (let i = 0, start = 0, end = 10; i < (expr.length / 10); i++ , start += 10, end += 10) {
+        arrayWithCodeLetters.push(expr.slice(start, end));
+    }
+
+    // compile our result with reduce method
+
+    let initialValue = '';
+    let result = arrayWithCodeLetters.reduce(
+        (accumulator, currentValue) => accumulator + objEncode[currentValue],
+        initialValue
+    );
+    return result;
 }
 
 module.exports = {
